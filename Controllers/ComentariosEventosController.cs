75b04b5d594cf66c941e99173b511da18bc4dd5d
Danielle Dias <danielle.c.santos9@aluno.senai.br>
2025-06-05 16:46:12 -0300

    feat: atualizando a api

--- Controllers/ComentariosEventosController.cs
+++ Controllers/ComentariosEventosController.cs
@@ -3,6 +3,7 @@ using Azure;
 using Azure.AI.ContentSafety;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using webapi.event_.Contexts;
 using webapi.event_.Domains;
 using webapi.event_.Interfaces;
 using webapi.event_.Repositories;
@@ -16,23 +17,39 @@ namespace webapi.event_.Controllers
     {
         private readonly IComentariosEventosRepository _comentarioseventosRepository;
         private readonly ContentSafetyClient _contentSafetyClient;
-        public ComentariosEventosController(ContentSafetyClient contentSafetyClient, IComentariosEventosRepository comentariosEventosRepository)
+        private readonly Context _context;
+        public ComentariosEventosController(ContentSafetyClient contentSafetyClient, IComentariosEventosRepository comentariosEventosRepository, Context contexto)
         {
             _comentarioseventosRepository = comentariosEventosRepository;
             _contentSafetyClient = contentSafetyClient;
+            _context = contexto;
         }
         [HttpPost]
         //o task representa uma funcao assincrona 
         public async Task<IActionResult> Post(ComentariosEventos comentario)
         {
 
-            try 
-	         {
-                //indica se a string(valor) eh vazia ou nula
+            try
+            {
+                Eventos eventoBuscado = _context.Eventos.FirstOrDefault(e => e.IdEvento == comentario.IdEvento);
+
+                if (eventoBuscado == null)
+                {
+                    return NotFound("Evento não encontrado");
+                }
+
+                if (eventoBuscado.DataEvento >= DateTime.UtcNow)
+                {
+                    return BadRequest("Não é possível comentar um evento que ainda não aconteceu");
+                }
+
+
+
                 if (string.IsNullOrEmpty(comentario.Descricao))
                 {
-                    return BadRequest("O texto a ser moderado nao pode estar vazio!!");
+                    return BadRequest("O texto a ser moderado nao pode estar vazio");
                 }
+
 
                 //criar um objeto de analise do content safety 
                 var request = new AnalyzeTextOptions(comentario.Descricao);
--- obj/Debug/net8.0/webapi.event+.AssemblyInfo.cs
+++ obj/Debug/net8.0/webapi.event+.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi.event+")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c19aabe45e21bed7b4cea2b9066dec4a46d08a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7627311797f9acbd0a5e08e5d0ab260a2876c84")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi.event+")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi.event+")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
